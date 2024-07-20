e1e43a4a73f64d4f4c99279b9c91d8377d1d2367
victor <victorayomide32@gmail.com>
2024-07-20 21:27:49 +0100

    removed dist from tracking

--- dist/routes/search.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const constants_1 = require("../constants");
-const router = (0, express_1.Router)();
-router.get("/quick", (req, res, next) => {
-    res.status(200).jsonp(constants_1.entries);
-});
-exports.default = router;
